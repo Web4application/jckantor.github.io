e8a626cab9f0eba343ecff396d693dacde515b7e
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-26 20:20:58 +0100

    Create Docker.ml

---
+++ tools/dev/Docker.ml
@@ -0,0 +1,20 @@
+BUILDPLATFORM — matches the current machine. (e.g. linux/amd64)
+
+BUILDOS — os component of BUILDPLATFORM, e.g. linux
+
+BUILDARCH — e.g. amd64, arm64, riscv64
+
+BUILDVARIANT — used to set ARM variant, e.g. v7
+
+TARGETPLATFORM — The value set with --platform flag on build
+
+TARGETOS - OS component from --platform, e.g. linux
+
+TARGETARCH - Architecture from --platform, e.g. arm64
+
+TARGETVARIANT
+FROM --platform=$BUILDPLATFORM alpine AS build
+# RUN <install build dependecies/compiler>
+# COPY <source> .
+ARG TARGETPLATFORM
+RUN compile --target=$TARGETPLATFORM -o /out/mybinary
